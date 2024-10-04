4a39a05891d1fa15696e657f5cd804d41c842ee0
Muhammad Nur Annas <44393156+annassdan@users.noreply.github.com>
2024-10-04 23:00:59 +0700

    Update package-fn.nix

--- ruby/package-fn.nix
+++ ruby/package-fn.nix
@@ -72,7 +72,7 @@ let
         (op fiddleSupport libffi)
         ++ (ops cursesSupport [ ncurses readline ])
         ++ (op docSupport groff)
-        ++ (op false zlib)
+        ++ (op zlibSupport zlib)
         ++ (op opensslSupport openssl)
         ++ (op gdbmSupport gdbm)
         ++ (op yamlSupport libyaml)
